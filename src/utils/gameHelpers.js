251273178d7fd068894b97f27dca91227d71b527
Sarah <sarahestrada33@gmail.com>
2026-05-29 21:39:45 -0600

    añadiendo constantes y helpers para el juego de snake

---
+++ src/utils/gameHelpers.js
@@ -0,0 +1,27 @@
+export const BOARD_SIZE = 20;
+export const INITIAL_SNAKE = [{ x: 10, y: 10 }];
+export const INITIAL_DIRECTION = { x: 0, y: -1 };
+export const INITIAL_SPEED = 150;
+
+export function getRandomFood(snake) {
+  let pos;
+  do {
+    pos = {
+      x: Math.floor(Math.random() * BOARD_SIZE),
+      y: Math.floor(Math.random() * BOARD_SIZE),
+    };
+  } while (snake.some((s) => s.x === pos.x && s.y === pos.y));
+  return pos;
+}
+
+export function checkCollision(head, snake) {
+  if (
+    head.x < 0 ||
+    head.x >= BOARD_SIZE ||
+    head.y < 0 ||
+    head.y >= BOARD_SIZE
+  ) {
+    return true;
+  }
+  return snake.slice(1).some((s) => s.x === head.x && s.y === head.y);
+}
